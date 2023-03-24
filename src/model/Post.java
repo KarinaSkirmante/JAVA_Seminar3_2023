97478943899d5805853e53f5f723a224cdd5b6a1
karina.skirmante <karina.krinkele@venta.lv>
2023-03-24 13:51:13 +0200

    Add: post class with variables and sets and gets

--- src/model/Post.java
+++ src/model/Post.java
@@ -1,5 +1,43 @@
 package model;
 
-public class Post {
+import java.time.LocalDateTime;
 
+public class Post {
+	//1. variables
+	private String msg;
+	private LocalDateTime dateTime;
+	//TODO need to change to Arraylist of User, which set the like to this post
+	private int countOfLikes;
+	
+	//2.getters and setters
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String inputMsg) {
+		if(inputMsg!=null && inputMsg.length() > 3) {
+			msg = inputMsg;
+		}
+		else
+		{
+			msg = "----";
+		}
+	}
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+	public void setDateTime() {
+		this.dateTime = LocalDateTime.now();
+	}
+	public int getCountOfLikes() {
+		return countOfLikes;
+	}
+	public void increaseLikes() {
+		countOfLikes++;
+	}
+	
+
+	
+	//3. constructors
+	
+	//4. toString
 }

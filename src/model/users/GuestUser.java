a68169c017d547e87b5f2cca65aff961355b9fed
karina.skirmante <karina.krinkele@venta.lv>
2023-03-24 14:51:00 +0200

    Add: new class Gueast user

---
+++ src/model/users/GuestUser.java
@@ -0,0 +1,26 @@
+package model.users;
+
+public class GuestUser {
+	private long generatedId;
+	private static long idCounter = 0;
+	
+	public long getGeneratedId() {
+		return generatedId;
+	}
+	public void setGeneratedId() {
+		this.generatedId = idCounter++;
+	}
+	
+	public GuestUser() {
+		setGeneratedId();
+	}
+		
+	public String toString() {
+		return "Guest User Nr." + generatedId;
+	}
+	
+	//TODO add find fuctions
+	
+	
+	
+}

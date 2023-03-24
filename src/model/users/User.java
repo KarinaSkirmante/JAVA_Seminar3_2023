b06e0f802dccfd7f3792615cc6946dbd7218bb1a
karina.skirmante <karina.krinkele@venta.lv>
2023-03-24 15:09:33 +0200

    Add: new class User with variables and setters and getters

--- src/model/users/User.java
+++ src/model/users/User.java
@@ -1,5 +1,53 @@
 package model.users;
 
-public class User {
+public abstract class User {
+	private String username;
+	private String encodedPassword;
+	private String name;//for Business User it will be name of owner
+	private String surname;
+	
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String inputUsername) {
+		if(inputUsername != null && inputUsername.matches("[a-z0-9.]{8,20}")) {
+			username = inputUsername;
+		}
+		else
+		{
+			username = "default.user";
+		}
+	}
+	public String getEncodedPassword() {
+		return encodedPassword;
+	}
+	public void setEncodedPassword(String inputEncodedPassword) {
+		if(inputEncodedPassword != null && inputEncodedPassword.matches("[A-Za-z0-9]{8, 20}")) {
+			//TODO find out simple password encoder 
+			encodedPassword = inputEncodedPassword;
+		}
+		else
+		{
+			encodedPassword = "defaultpassword";
+		}
+		
+	}
+	//TODO verification in setName and setSurname
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	
+	
 
 }

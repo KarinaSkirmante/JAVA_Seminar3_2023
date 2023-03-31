04e668c8323a5e6bf956836a90cc1aa3d8b3c641
Karina Šķirmante <karina.krinkele@vea.lv>
2023-03-31 13:56:00 +0300

    Edir: mainService added user objects

--- src/service/MainService.java
+++ src/service/MainService.java
@@ -2,6 +2,7 @@ package service;
 
 import java.util.ArrayList;
 
+import model.users.BussinessUser;
 import model.users.GuestUser;
 import model.users.PrivateUser;
 import model.users.User;
@@ -19,9 +20,24 @@ public class MainService {
 		
 		System.out.println(u1);
 		System.out.println(u2);
+		
+		//TODO create register function for users 
+		//TODO create UserTYpe as enum - privateUser, businessUser
+		//public boolean register(String name, String surname, String username, String password, UserType userType)
+		
 		//2. create 2 Private user objects
-		PrivateUser u3 = new User();
+		PrivateUser u3 = new PrivateUser("Karina", "Skirmante", "karina.skirmante", "123");
+		PrivateUser u4 = new PrivateUser("Janis", "Berzins", "janis.berzins", "321");
+		allRegisterdUsers.add(u3);
+		allRegisterdUsers.add(u4);
+		
 		//3. create 2 Business User objects
+		
+		BussinessUser u5 = new BussinessUser("SIA", "Ziedi", "sia.ziedi", "987");
+		BussinessUser u6 = new BussinessUser("SIA", "Dators", "sia.dators", "789");
+		allRegisterdUsers.add(u5);
+		allRegisterdUsers.add(u6);
+		
 		//4. create 1 private and 1 public posts for each Private User
 		//5. create page for private user --> it is not allowed in our system
 		//6. create at least one page for each Business User

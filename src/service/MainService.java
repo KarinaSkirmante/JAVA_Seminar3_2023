6d516d9e6e29a0ebe4c2322e0a5a2ded47e83163
Karina Šķirmante <karina.krinkele@vea.lv>
2023-03-31 13:32:26 +0300

    Edit: setDateTime in Post class and TODO in service

--- src/service/MainService.java
+++ src/service/MainService.java
@@ -10,7 +10,20 @@ public class MainService {
 	
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO 
+		//1. create 2 Guest User objects
+		//2. create 2 Private user objects
+		//3. create 2 Business User objects
+		//4. create 1 private and 1 public posts for each Private User
+		//5. create page for private user --> it is not allowed in our system
+		//6. create at least one page for each Business User
+		//7. create at least one post in each page
+		
+		//8. verify login func.
+		//9. verify followPage func.
+		//10. verify addFollower func.
+		//11. verify removeFollower func.
+		//12. verify increaseLikes func.
 
 	}
 
